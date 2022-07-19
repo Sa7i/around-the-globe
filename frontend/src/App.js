be8b7041e49d976a93127eb88ae4133628830f6c
Sa7i <joao-sassetti_student2022@wilder.school>
2022-07-19 13:59:11 +0100

    setting up react app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="font-bold">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
